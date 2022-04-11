639ea251eef59746cf32672e4b22f5b501cb7aef
Ismael Pires <ismaellpires@gmail.com>
2022-04-11 19:07:56 -0300

    Atualizada bibliotecas

--- src/views/index.js
+++ src/views/index.js
@@ -1,25 +1,22 @@
-import React from 'react'
-import {BrowserRouter, Route, Switch, Redirect} from "react-router-dom";
+import React from "react"
+import {BrowserRouter, Route, Switch, Redirect} from "react-router-dom"
 
-import Flex from './Flex'
-import Forms from './Forms'
+import Flex from "./Flex"
+import Forms from "./Forms"
 
 const App = () => {
 
     return (
         <BrowserRouter>
             <section id="main">
-                <Switch>
-                    <Route
-                        path="/flex"
-                        render={props => <Flex {...props} />}
-                    />
-                    <Route
-                        path="/forms"
-                        render={props => <Forms {...props} />}
-                    />
-                    <Redirect to="/flex"/>
-                </Switch>
+                <Route
+                    path="/flex"
+                    render={props => <Flex {...props} />}
+                />
+                <Route
+                    path="/forms"
+                    render={props => <Forms {...props} />}
+                />
             </section>
         </BrowserRouter>
     )
